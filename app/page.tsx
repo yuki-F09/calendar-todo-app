7357f7b88a25574ecb52ec8d42197285da4f0a43
Yu_ki <yu_ki1309@outlook.jp>
2026-06-05 08:01:24 +0900

    fe:full calendarの導入のみ

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import Calendar from "@/components/calendar";
 
 export default function Home() {
   return (
     <>
       <h1>メインページ</h1>
+
+      <div className="mt-10 mx-10">
+        <Calendar/>
+
+      </div>
     </>
   )
 }
