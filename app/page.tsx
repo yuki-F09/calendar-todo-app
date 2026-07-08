e758717793f4cdfd2805c444e82d5b05f064e192
Yu_ki <yu_ki1309@outlook.jp>
2026-07-08 20:31:05 +0900

    fe calendar画面にtaskを表示

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,25 @@
 import Calendar from "@/components/calendar";
 
-export default function Home() {
+import { createClient } from '@/lib/supabase/server'
+import { prisma } from '@/lib/prisma'
+
+export default async function Home() {
+  const supabase = await createClient()
+  const { data: { user } } = await supabase.auth.getUser()
+
+  const [tags, tasks, tagPrioritys] = user
+    ? await Promise.all([
+        prisma.tag.findMany({ where: { auth_id: user.id } }),
+        prisma.task.findMany({ where: { auth_id: user.id }, include: { tags: true } }),
+        prisma.tagPriority.findMany({where: {auth_id: user.id}}),
+      ])
+    : [[], []]
+
   return (
     <>
 
       <div className="mt-10 mx-10">
-        <Calendar/>
+        <Calendar tasks={tasks} tags={tags}/>
       </div>
     </>
   )
--- components/calendar/index.tsx
+++ components/calendar/index.tsx
@@ -5,14 +5,30 @@ import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from '@fullcalendar/interaction';
 import { useRouter } from 'next/navigation';
 
-export default function Calendar() {
+
+
+type Tag = { id: number; tag_name: string; color: string }
+type Task = { id: number; title: string; description: string | null; role_over: boolean; tags: Tag[]; date:string; }
+
+type Props = {
+  tasks: Task[]
+  tags: Tag[]
+}
+
+export default function Calendar({tags, tasks} :Props) {
   const router = useRouter();
+  const events = tasks.map(({ id, title, date }) => ({
+    id: String(id),                                                                                           
+    title,                                                                                                    
+    start: date,                                                                                          
+  }))                    
 
   return (
     <FullCalendar
       plugins={[ dayGridPlugin, interactionPlugin ]}
       initialView="dayGridMonth"
       dateClick={(info) => router.push(`/${info.dateStr}`)}
+      events={events}
     />
   )
 }
