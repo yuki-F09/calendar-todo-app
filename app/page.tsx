855bbd429fc900d3c289833004642c95b0b46557
Yu_ki <yu_ki1309@outlook.jp>
2026-07-09 10:43:28 +0900

    fe tagPriorityによる並び替え対応

--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,18 @@ export default async function Home() {
   const supabase = await createClient()
   const { data: { user } } = await supabase.auth.getUser()
 
-  const [tags, tasks, tagPrioritys] = user
+  const [ tasks, tagPriorities ] = user
     ? await Promise.all([
-        prisma.tag.findMany({ where: { auth_id: user.id } }),
         prisma.task.findMany({ where: { auth_id: user.id }, include: { tags: true } }),
         prisma.tagPriority.findMany({where: {auth_id: user.id}}),
       ])
-    : [[], []]
+    : [[], [], []]
 
   return (
     <>
 
       <div className="mt-10 mx-10">
-        <Calendar tasks={tasks} tags={tags}/>
+        <Calendar tasks={tasks} tagPriorities={tagPriorities}/>
       </div>
     </>
   )
--- components/calendar/index.tsx
+++ components/calendar/index.tsx
@@ -8,24 +8,32 @@ import { Hex_Color_Map } from '@/components/ui/tag';
 import type { TagColor } from '@/lib/generated/prisma/enums';
 
 
-
 type Tag = { id: number; tag_name: string; color: string }
-type Task = { id: number; title: string; description: string | null; role_over: boolean; tags: Tag[]; date:string; }
+type Task = { id: number; title: string; description: string | null; role_over: boolean; tags: Tag[]; date:string;}
+type tagPriority = {id: number, priority: number, auth_id: string, tag_id: number }
+
 
 type Props = {
   tasks: Task[]
-  tags: Tag[]
+  tagPriorities: tagPriority[]
 }
 
-export default function Calendar({tags, tasks} :Props) {
+export default function Calendar({tasks, tagPriorities} :Props) {
   const router = useRouter();
-  const events = tasks.map(({ id, title, date, tags }) => ({
+  const events = tasks.map(({ id, title, date, tags }) => {
+    // 複数tagのときの順番に未対応 ??は左側がnullかundefinedで右を返す
+    const priority = tagPriorities.find(tp => tp.tag_id === tags[0]?.id)?.priority ?? Infinity
+    return{
     id: String(id),
     title,
     start: date,
+    extendedProps: {priority},
     backgroundColor: tags[0] ? Hex_Color_Map[tags[0].color as TagColor] : "#4a4a4f ",
     borderColor: tags[0] ? Hex_Color_Map[tags[0].color as TagColor] : "#4a4a4f" ,
-  }))                    
+    }
+
+  }
+)                    
 
   return (
     <FullCalendar
@@ -33,6 +41,11 @@ export default function Calendar({tags, tasks} :Props) {
       initialView="dayGridMonth"
       dateClick={(info) => router.push(`/${info.dateStr}`)}
       events={events}
+       eventOrder={(a: unknown, b: unknown) => {                                                                   
+    const pa = (a as { extendedProps: { priority: number } }).extendedProps.priority ?? Infinity
+    const pb = (b as { extendedProps: { priority: number } }).extendedProps.priority ?? Infinity              
+    return pa - pb                                                                                            
+  }}    
     />
   )
 }
